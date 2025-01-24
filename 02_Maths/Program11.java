1daad97f21d5c5a2a65a03b14e65ba434898cc28
Sachin Shetkar <97154362+SachinSS45@users.noreply.github.com>
2025-01-24 12:30:34 +0530

    Count Prime in Range

--- 02_Maths/Program11.java
+++ 02_Maths/Program11.java
@@ -2,3 +2,36 @@
  * Q.11 : Count Primes In Range : 
  * GFG Link : https://www.geeksforgeeks.org/problems/count-primes-in-range1604/0
  */
+
+class Solution {
+    int countPrimes(int L, int R) {
+        // code here
+        int[] prime = new int[R+1];
+        
+        for(int i=2;i<=R;i++){
+            prime[i] = 1;
+        }
+        
+        for(int i=2;i*i<=R;i++){
+            if(prime[i]==1){
+                
+                if(i>Integer.MAX_VALUE/i){
+                    continue;
+                }
+                int j = i*i;
+                
+                while(j<=R){
+                    prime[j] = 0;
+                    j+=i;
+                }
+            }
+        }
+        int count = 0;
+        for(int i=L;i<=R;i++){
+            if(prime[i]==1){
+                count++;
+            }
+        }
+        return count;
+    }
+};
